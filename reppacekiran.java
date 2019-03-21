160ed4d439e504b60c8ceb72538efe850df3c175
kiranreddy <srikiranreddy09@gmail.com>
2019-03-21 15:39:53 +0530

    Commit from my side

---
+++ reppacekiran.java
@@ -0,0 +1,43 @@
+kiran first git practice 
+kiran java file is ready 
+Git hub is having three stages working area statging area local reporsity
+
+once file is created it will come under WA
+once you add that file it will move from WA to SA 
+After commit the file will move from SA to LR
+
+how to add file into SA
+git add file name if suppose we have more files then git add * or git add *.java 
+as per out requirement we can modify the git add command 
+once add is done you can see the file in SA by using command call git status 
+
+on evert stage identificaiton we can use git status is the command 
+once add is done then command to move from SA to LA
+commit -m "msg content" file name SR->LR
+once it is done now its time to update the same files into Remove repository
+
+first we need to come the remote reporsitry
+
+git remote add alias name "url"
+to check whether it is connect propertly or not we need to check by using the command called 
+git remote -v
+
+how to check gitbash veriosn git --verison 
+how to check the git log 
+git log 
+suppose if the file is created and uploaded in RR 
+
+again you have made some changed then we need to use commanad 
+
+git comment -a -m "updated  a means add m message content  this for exiting file and modified file .
+
+
+
+WA files will come over red color untracked files 
+SA green  tracked files but not commited yet
+LA on commit to brach master
+
+
+github version in prod 2.16.4 
+
+
